12a7b03b4fdd896b865f2c0390d8f2be2a2c823b
imon001 <bishojitdasemon01@gmail.com>
2023-12-05 02:37:55 +0600

    state_provider riverpod

--- lib/provider/provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final stringProvider = Provider((ref) {
-  return "testing riverpod provider ";
-});
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:project_one/provider/provider.dart';
+import 'package:project_one/provider/state_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final testString = ref.watch(stringProvider);
     return Scaffold(
-      body: Center(
-        child: Text(testString),
+      body: Consumer(
+        builder: (context, ref, child) {
+          int count = ref.watch(dataProvider);
+          return Center(
+            child: Text(count.toString()),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(dataProvider.notifier).state++;
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
